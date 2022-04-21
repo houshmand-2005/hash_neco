227e64f6bb8111e3ff5582835d8114523307526b
houshmand <houshmand2005@gmail.com>
2022-04-21 14:32:31 +0430

    just create django project

---
+++ hash_neco/hash_neco/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for hash_neco project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hash_neco.settings')
+
+application = get_wsgi_application()
