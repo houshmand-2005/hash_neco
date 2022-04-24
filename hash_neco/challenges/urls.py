8346002f65e30c416c87e92d2ba5b6260488c4ba
houshmand <houshmand2005@gmail.com>
2022-04-24 10:18:08 +0430

    use Dynamic Path URL

--- hash_neco/challenges/urls.py
+++ hash_neco/challenges/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.index, name="january"),
-    path("february", views.index_feb, name="february"),  # 005
+    # path("january", views.index, name="january"),
+    # path("february", views.index_feb, name="february"),  # 005
+    path("<month>", views.month_index, name="month_index"),  # 006
 ]
