241b717b11dc671b870a429cfa97ff0ea2a264e8
houshmand <houshmand2005@gmail.com>
2022-04-24 13:42:45 +0430

    add str: and int: to url pramater

--- hash_neco/challenges/urls.py
+++ hash_neco/challenges/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # path("january", views.index, name="january"),
     # path("february", views.index_feb, name="february"),  # 005
-    path("<month>", views.month_index, name="month_index"),  # 006
+    path("<int:month>", views.month_index_bynumber, name="month_index"),  # 007
+    path("<str:month>", views.month_index, name="month_index"),  # 006
 ]
