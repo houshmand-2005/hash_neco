339c74d87ed7f6e313200efef50dd32331a87c11
houshmand <houshmand2005@gmail.com>
2022-04-29 16:08:56 +0430

    add root path for challenges

--- hash_neco/challenges/urls.py
+++ hash_neco/challenges/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # path("february", views.index_feb, name="february"),  # 005
     path("<int:month>", views.month_index_bynumber, name="month_index"),  # 007
     path("<str:month>", views.month_index, name="month_index"),  # 006
+    path("", views.chall_index, name="index"),
 ]
