64de4b492c068d5bc383a29af5165f7c08876684
houshmand <houshmand2005@gmail.com>
2022-05-05 17:03:00 +0430

    now return an html file with templates

--- hash_neco/challenges/views.py
+++ hash_neco/challenges/views.py
@@ -2,17 +2,9 @@ from urllib import response
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 # Create your views here.
 
-
-# def index(request):
-#     return HttpResponse("Hello, world. You're at the hash_neco index.")
-
-
-# def index_feb(request):
-#     return HttpResponse("this is february challenge:)")  # 005
-
-# 006
 
 month_challenge = {
     "january": "this is january challenge:)",
@@ -22,23 +14,12 @@ month_challenge = {
 
 
 def month_index(request, month):
-    # if month == "january":
-    #     challenge_text = "this is january challenge:)"
-    # elif month == "february":
-    #     challenge_text = "this is february challenge:)"
-    # elif month == "march":
-    #     challenge_text = "this is march challenge:)"
-    # else:
-    #     return HttpResponseNotFound("<h1>404</h1>")
-    # return HttpResponse(challenge_text)
-    #  008
     try:
         challenge_text = month_challenge[month]
-        response_data = f"<h1>{challenge_text}</h1>"
+        response_data = render_to_string("challenges/challenge.html")
         return HttpResponse(response_data)
     except:
         return HttpResponseNotFound("<h1>404</h1>")
-# 007 and 008
 
 
 def month_index_bynumber(request, month):
