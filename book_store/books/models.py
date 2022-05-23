d4e20306b9ccf81e93b83ff1da8bef6960a208b5
houshmand <houshmand2005@gmail.com>
2022-05-23 17:23:58 +0430

    add anthor and rating validator and best seller prammter

--- book_store/books/models.py
+++ book_store/books/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    another = models.CharField(max_length=100, null=True)
+    is_best_seller = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.title} - ({self.rating})'
