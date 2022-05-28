ce61362be54012045c73e84d92a0d6316c5d8128
houshmand <houshmand2005@gmail.com>
2022-05-28 18:06:50 +0430

    query books and render in to templates

--- book_store/books/views.py
+++ book_store/books/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Book
 # Create your views here.
+def index(request):
+    books = Book.objects.all()
+    return render(request, 'book_outlet/index.html', {
+        "books": books
+    })
