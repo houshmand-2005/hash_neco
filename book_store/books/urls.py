1561a2bd94f5553e79de5e3bf9c4ac4fc2def9d8
houshmand <houshmand2005@gmail.com>
2022-05-29 22:00:47 +0430

    change int<> to slug segment

--- book_store/books/urls.py
+++ book_store/books/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path("<int:id>", views.book_page, name="book_page")
+    path("<slug:slug>", views.book_page, name="book_page")
 ]
