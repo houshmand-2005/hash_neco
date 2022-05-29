5c28b4105ea288814923756d469247d47d6fa288
houshmand <houshmand2005@gmail.com>
2022-05-29 13:44:58 +0430

    write view for book page

--- book_store/books/views.py
+++ book_store/books/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 
 from .models import Book
+from django.http import Http404
 # Create your views here.
+
+
 def index(request):
     books = Book.objects.all()
     return render(request, 'book_outlet/index.html', {
         "books": books
+    })
+
+
+def book_page(request, id):
+    # try:
+    #     book = Book.objects.get(pk=id)
+    # except:
+    #     raise Http404()
+    book = get_object_or_404(Book, pk=id)
+    return render(request, 'book_outlet/book_page.html', {
+        "title": book.title,
+        "another": book.another,
+        "reting": book.rating,
+        "is_best_seller": book.is_best_seller
     })
