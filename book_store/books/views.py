51790de9cf7ad857deb900c7e1c5ea3c470c2eaa
houshmand <houshmand2005@gmail.com>
2022-05-29 22:00:21 +0430

    now books link with slug and order z to a

--- book_store/books/views.py
+++ book_store/books/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render, get_object_or_404 
 from .models import Book
 from django.http import Http404
+from django.db.models import Avg
 # Create your views here.
 
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("-title")
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg('rating'))
     return render(request, 'book_outlet/index.html', {
-        "books": books
+        "books": books,
+        "total_count": num_books,
+        "avg_rating": avg_rating
     })
 
 
-def book_page(request, id):
+def book_page(request, slug):
     # try:
     #     book = Book.objects.get(pk=id)
     # except:
     #     raise Http404()
-    book = get_object_or_404(Book, pk=id)
+    book = get_object_or_404(Book, slug=slug)
     return render(request, 'book_outlet/book_page.html', {
         "title": book.title,
         "another": book.another,
