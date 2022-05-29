5c06f057b3ea61bddaf08a0ca58628f143ecfc75
houshmand <houshmand2005@gmail.com>
2022-05-29 16:41:11 +0430

    add get_absolute_url

--- book_store/books/models.py
+++ book_store/books/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-
+from django.urls import reverse
 # Create your models here.
 
 
@@ -10,6 +10,9 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     another = models.CharField(max_length=100, null=True)
     is_best_seller = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse("book_page", args=[self.id])
 
     def __str__(self):
         return f'{self.title} - ({self.rating})'
