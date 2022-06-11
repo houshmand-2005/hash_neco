3c1f3df014e6a69888a67734117d5b9c9a2cf3ad
houshmand <houshmand2005@gmail.com>
2022-06-11 22:41:41 +0430

    add another field to admin panel

--- book_store/books/admin.py
+++ book_store/books/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Another
 # Register your models here.
 
 
@@ -10,3 +10,4 @@ class BookAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Another)
