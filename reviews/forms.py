dd92aaad89604972b10fbcc4a5b7cd1ef5d50aaf
houshmand <houshmand2005@gmail.com>
2022-06-16 12:39:24 +0430

    just add some customization for this input like error meseges and more

--- reviews/forms.py
+++ reviews/forms.py
@@ -2,4 +2,8 @@ from django import forms
 
 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField()
+    user_name = forms.CharField(label="Your Name", max_length=20, error_messages={
+        "required": "Your name must not be empty",
+        "max_length": "please enter a shorter name!"
+    })
+
