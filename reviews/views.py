10b2aa054aa6f9847d8e5ff02b521a9ca040e976
houshmand <houshmand2005@gmail.com>
2022-06-19 21:07:40 +0430

    save entered data by user in data base

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import ReviewForm
+from .models import Review
 
 
 # Create your views here.
@@ -8,7 +9,9 @@ def review(request):
     if request.method == "POST":
         form = ReviewForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            review_get = Review(user_name=form.cleaned_data['user_name'], review_text=form.cleaned_data['review_text'],
+                                rating=form.cleaned_data['rating'])
+            review_get.save()
             return HttpResponseRedirect("/thank_you")
 
     else:
