fffdc51d1bb8cddbf127f81682e705fc45885978
houshmand <houshmand2005@gmail.com>
2022-06-19 23:11:30 +0430

    add error message and label to field

--- reviews/forms.py
+++ reviews/forms.py
@@ -14,3 +14,14 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = '__all__'  # ['user_name', 'review_text', 'rating'] exclude = ['something']
+        labels = {
+            "user_name": "Your Name",
+            "review_text": "Your Feedback",
+            "rating": "Your Rating"
+        }
+        error_messages = {
+            "user_name": {
+                "required": "Your name must not be empty",
+                "max_length": "please enter a shorter name!"
+            }
+        }
