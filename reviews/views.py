4f7ab586e7a9d477480775093447d4b14071ac46
houshmand <houshmand2005@gmail.com>
2022-06-20 14:16:17 +0430

    change view to class baesd view

--- reviews/views.py
+++ reviews/views.py
@@ -2,25 +2,26 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import ReviewForm
 from .models import Review
+from django.views import View
+
+
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+        return render(request, "reviews/reviews.html", {
+            "form": form
+        })
+
+    def post(self, request):
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/thank_you")
+        return render(request, "reviews/reviews.html", {
+            "form": form
+        })
 
 
 # Create your views here.
-def review(request):
-    if request.method == "POST":
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            # review_get = Review(user_name=form.cleaned_data['user_name'], review_text=form.cleaned_data[
-            # 'review_text'], rating=form.cleaned_data['rating'])
-            form.save()
-            return HttpResponseRedirect("/thank_you")
-
-    else:
-        form = ReviewForm()
-
-    return render(request, "reviews/reviews.html", {
-        "form": form
-    })
-
-
 def thank_you(request):
     return render(request, "reviews/thank_you.html")
