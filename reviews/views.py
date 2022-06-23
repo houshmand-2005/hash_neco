6a7dad4b660792cc284649d4ecaf73febfc5f53d
houshmand <houshmand2005@gmail.com>
2022-06-23 13:35:30 +0430

    change thank you view to class view and add a view for show al of the reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 from .forms import ReviewForm
 from .models import Review
 from django.views import View
@@ -22,6 +24,20 @@ class ReviewView(View):
         })
 
 
-# Create your views here.
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+class ThankYou(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This works!"
+        return context
+
+
+class ReviewList(TemplateView):
+    template_name = "reviews/review_list.html"
+
+    def get_context_data(self, **kwargs):
+        reviews = Review.objects.all()
+        context = super().get_context_data(**kwargs)
+        context["reviews"] = reviews
+        return context
