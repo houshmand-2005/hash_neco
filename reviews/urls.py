bdc44290e4b381f70a8c1b2f5f0e6c3f44ab88e7
houshmand <houshmand2005@gmail.com>
2022-06-23 13:36:10 +0430

    add a url for reviewlist

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.ReviewView.as_view()),
-    path("thank_you", views.thank_you)
+    path("thank_you", views.ThankYou.as_view()),
+    path("reviews", views.ReviewList.as_view())
 ]
