ec299e2ac77f5b1c9b3239af8ec0b51783c0057c
houshmand <houshmand2005@gmail.com>
2022-06-25 19:39:57 +0430

    add a function for store photo

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+
+def store_file(file):
+    with open("temp/image.png", "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
 
 
 class CreateProfileView(View):
@@ -9,4 +16,5 @@ class CreateProfileView(View):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-        pass
+        store_file(request.FILES["image"])
+        return HttpResponseRedirect("/profiles")
