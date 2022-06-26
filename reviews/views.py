4850f71fe7c106ba345989723a5b5e70057c24ed
houshmand <houshmand2005@gmail.com>
2022-06-26 21:20:28 +0430

    use seesions

--- reviews/views.py
+++ reviews/views.py
@@ -57,6 +57,14 @@ class DetailReview(DetailView):
     template_name = "reviews/detail_review.html"
     model = Review
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_review = self.object
+        request = self.request
+        favorite_id = request.session.get("favorite_review")
+        context["is_favorite"] = favorite_id == str(loaded_review.id)
+        return context
+
 
 class AddFavoriteView(View):
     def post(self, request):
