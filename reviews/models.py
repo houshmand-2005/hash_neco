d3cab6f955b380e1c5771acfd743ab6462d37459
Houshmand <77535700+houshmand-2005@users.noreply.github.com>
2022-06-27 10:53:40 +0430

    del

--- reviews/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class Review(models.Model):
-    user_name = models.CharField(max_length=20)
-    review_text = models.TextField()
-    rating = models.IntegerField()
--- reviews/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path("", views.ReviewView.as_view()),
-    path("thank_you", views.ThankYou.as_view()),
-    path("reviews", views.ReviewList.as_view()),
-    path("reviews/favorite", views.AddFavoriteView.as_view()),
-    path("reviews/<int:pk>", views.DetailReview.as_view(), name="reviews_page")
-]
